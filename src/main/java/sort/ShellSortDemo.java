d2e0e580a6429b93f783ddfba2238c8746d7714a
cyf2018 <871263057@qq.com>
2020-06-03 16:57:41 +0800

    希尔排序算法-移位法（优化）

--- src/main/java/sort/ShellSortDemo.java
+++ src/main/java/sort/ShellSortDemo.java
@@ -1,6 +1,6 @@
 package sort;
 
-import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 希尔排序
@@ -14,9 +14,42 @@ public class ShellSortDemo {
 
         int[] arr = {3, 1, 2, 5, 4, 6, 9, 8, 7, 0};
 
-        // 希尔排序
-        shellSort(arr);
 
+        // 希尔排序
+        int[] randomArr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            randomArr[i] = new Random().nextInt(80000);
+        }
+        //System.out.println(Arrays.toString(randomArr));
+
+        long startTime = System.currentTimeMillis();
+        //shellSort(randomArr);// 交换式
+        shellSort2(randomArr);// 移位式
+        System.out.println("shellSort 排序耗时 "+(System.currentTimeMillis()-startTime)+"ms"); //shellSort 排序耗时 7673ms
+        System.out.println("shellSort 排序耗时 "+(System.currentTimeMillis()-startTime)+"ms"); //shellSort 排序耗时 67ms
+
+        //System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    private static void shellSort2(int[] arr) {
+        // 优化 交换改为移位
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+
+            }
+        }
     }
 
     private static void shellSort(int[] arr) {
@@ -27,7 +60,7 @@ public class ShellSortDemo {
             for (int i = gap; i < arr.length; i++) {
 
                 for (int j = i - gap; j >= 0; j -= gap) {
-                    // 交换效率慢
+                    //交换效率慢
                     if (arr[j] > arr[j + gap]) {
                         temp = arr[j];
                         arr[j] = arr[j + gap];
@@ -35,8 +68,8 @@ public class ShellSortDemo {
                     }
                 }
             }
-            System.out.println("第" + (++count) + "轮后排序结果为");
-            System.out.println(Arrays.toString(arr));
+            //System.out.println("第" + (++count) + "轮后排序结果为");
+            //System.out.println(Arrays.toString(arr));
 
 
         }
